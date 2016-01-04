5c62f1100ed41f1e04b86622d63a6f23dc88d626
ClintRed <mouse198115@gmail.com>
2016-01-05 00:05:52 +0800

    initial

---
+++ js/myJquery.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    
+    $("#kv").animate({
+    	opacity:1,
+    	"margin-top":"+=100",
+
+    },1000)
+
+	$("nav").animate({
+    	"margin-top":"+=10",
+
+    },1500)
+
+	$("#about").click(function(){
+		$("#kv").fadeOut("slow");
+	})
+
+
+
+	
+
+});
