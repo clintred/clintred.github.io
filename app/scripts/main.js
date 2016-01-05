2fae17e66bb02de7e1a95472a058b3d4a66bb155
sexyoung <Habuche@gmail.com>
2016-01-05 21:23:33 +0800

    加入 slick

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,4 +14,19 @@ $(document).ready(function() {
   $('#about').click(function(){
     $('#kv').fadeOut('slow');
   });
+
+  $('.design.mainSection .wrapper:first').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false
+  });
+
+  $('.btn_left').on('click', function(){
+    $('.design.mainSection .wrapper:first').slick('slickPrev');
+  });
+
+  $('.btn_right').on('click', function(){
+    $('.design.mainSection .wrapper:first').slick('slickNext');
+  });
+
 });
