cd4cc2c2a00b274297c962668d5fa131efac7547
sexyoung <Habuche@gmail.com>
2016-01-05 14:35:52 +0800

    把東西重新整理一下 把 css 跟 js 都丟進 app 資料夾

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+
+$(document).ready(function() {
+    $("#kv").animate({
+      opacity:1,
+      "margin-top":"+=100",
+
+    },1000)
+
+  $("nav").animate({
+      "margin-top":"+=10",
+
+    },1500);
+
+  $("#about").click(function(){
+    $("#kv").fadeOut("slow");
+  });
+});
