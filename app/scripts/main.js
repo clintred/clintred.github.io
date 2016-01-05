027d48c4c608c731612728c251378632574af340
sexyoung <Habuche@gmail.com>
2016-01-05 23:29:21 +0800

    打包

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,6 @@
 'use strict';
 console.warn('develop');
 $(document).ready(function() {
-    $('#kv').animate({
-      opacity: 1,
-      'margin-top': '+=100'
-    }, 1000);
-
-  $('nav').animate({
-      'margin-top': '+=10'
-    }, 1500);
 
   $('#about').click(function(){
     $('#kv').fadeOut('slow');
